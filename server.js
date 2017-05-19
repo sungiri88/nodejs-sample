058458b538bc84dc00d5278bbf33c26e80ca0373
UMAPATHY GIRIRAJAN <sungiri88@yahoo.com>
2017-05-19 11:45:29 +0000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,14 @@
+var express=require('express')
+var app=express();
+
+
+app.get('/',function(req,res){
+res.send("Hello World");
+});
+
+app.listen('8080',function(){
+console.log("Listenting on 8080");
+});
+
+
+
